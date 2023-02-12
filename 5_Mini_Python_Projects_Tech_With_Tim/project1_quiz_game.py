dfc85414d20042803c0a4e2068f5304fb0c33bd2
Taepung <tayfunkamit@gmail.com>
2023-02-13 01:16:36 +0300

    project 2,3,4,5 files are created with explanation

--- 5_Mini_Python_Projects_Tech_With_Tim/project1_quiz_game.py
+++ 5_Mini_Python_Projects_Tech_With_Tim/project1_quiz_game.py
@@ -1,6 +1,8 @@
 """
 This is done watching the video 5 Mini Python Projects - For Beginners on https://www.youtube.com/watch?v=DLn3jOsNRVE
 I watched the videos and after that did the project not looking the actual codes.
+
+04:51 | Project #1 - Quiz Game
 """
 
 
---
+++ 5_Mini_Python_Projects_Tech_With_Tim/project3_rock_paper_scissors.py
@@ -0,0 +1,6 @@
+"""
+This is done watching the video 5 Mini Python Projects - For Beginners on https://www.youtube.com/watch?v=DLn3jOsNRVE
+I watched the videos and after that did the project not looking the actual codes.
+
+42:53 | Project #3 - Rock, Paper, Scissors
+"""
